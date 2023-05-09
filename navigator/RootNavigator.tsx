bea984697eabe078417fdf2cf76ae22dfebcfdde
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2023-05-09 10:11:48 +0800

    set up screens,tailwind,navigation,typedefs, graphql, apollo,create useOrders, useCustomerOrders hooks

---
+++ navigator/RootNavigator.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './TabNavigator';
+
+export type RootStackParamList = {
+  Main: undefined; // no props
+  MyModal: { userId: string; name: string};
+  Order: { order: any }; // todo: define order type
+}
+
+const RootStack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <RootStack.Navigator>
+      <RootStack.Group>
+        <RootStack.Screen name="Main" component={TabNavigator} />
+      </RootStack.Group>
+    </RootStack.Navigator>
+  )
+}
+export default RootNavigator
